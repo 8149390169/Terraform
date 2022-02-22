f867c5a53fbd544b976d8f16e83ab717c7d507db
shubham <shubhambhadke@gmail.com>
2022-02-22 13:14:12 +0530

    providers

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
 #  access_key = var.AWS_ACCESS_KEY
 #  secret_key = var.AWS_SECRET_KEY
   region = var.AWS_REGION
-  shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
+#  shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
 }
 
